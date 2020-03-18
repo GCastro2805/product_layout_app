3f767c73e01eb99c1404049fc14566250afc24e0
gcastro <gcaztro2805@hotmail.com>
2020-03-17 19:10:27 -0700

    Ver 202003171910

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Product layout demo homa page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      body: Center(
+          child: Text(
+        'Hello World',
+      )),
+    );
+  }
+}
+
+class ProductBox extends StatelessWidget{
+  ProductBox({Key key, this.name, this.description, this.price, this.image}) : super(key:key);
+
+  final String name;
+  final String description;
+  final int price;
+  final string image;
+
+  Widget build(BuildContext context){
+    return Container(
+      padding: EdgeInsets.all(2),
+      height: 120,
+      child: Card(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Image.asset("assets/appimages/" + image),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(5),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(this.name,
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                Text(this.description),
+                Text("Price: " + this.price.toString()),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+
+      ),
+    )
+  }
+}
